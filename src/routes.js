7c6af764c51e358ae7577b02d29db61aab96a94e
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-16 16:26:19 -0300

    using mysql

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const routes = new express.Router();
 const PageController = require("./controllers/PageController");
 
 routes.get("/", PageController.index);
+routes.get("/all", PageController.all);
+routes.get("/get/:id", PageController.get);
 
 module.exports = routes;
