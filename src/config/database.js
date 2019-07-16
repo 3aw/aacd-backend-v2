57160acf90f5d87681913aab90421273a0ac9f3c
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-16 18:26:40 -0300

    sequelized configured

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: "mariadb",
+  host: "127.0.0.1",
+  username: "root",
+  password: "",
+  database: "aacd-backend-v2",
+  operatorAliase: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
