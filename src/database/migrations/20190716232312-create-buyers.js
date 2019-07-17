2549597b482600a43a716c8f79dbb1b22fecb18c
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-16 23:50:07 -0300

    creating migration for buyers table

--- src/database/migrations/20190716232312-create-buyers.js
+++ src/database/migrations/20190716232312-create-buyers.js
@@ -1,15 +1,38 @@
 "use strict";
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {},
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("buyers", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable("buyers");
   }
 };
