7374fabbb1c1f794ba05e90cb8cfa5df92ca3a31
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-25 02:10:22 -0300

    paypal working good

--- src/app/controllers/PageController.js
+++ src/app/controllers/PageController.js
@@ -1,11 +1,86 @@
 const mysql = require("mysql");
+const paypal = require("paypal-rest-sdk");
 
 module.exports = {
-  async index(req, res) {
-    return res.send("Hello, World!");
+  async pay(req, res) {
+    paypal.configure({
+      mode: "sandbox",
+      client_id:
+        "ATcjnceGtqrltmuzNcuuXbiY4dtAuAXW31Lc9h2hraVdYYglTFMdHMhPkN0qrgZlKLAGbDPDV1wYfJjr",
+      client_secret:
+        "EE_aFgHqCTqfKVWtxgEY4CsQdO9yL3qaEOEK843iWU-HYv4GO1YPb9Kjoh3yUn0wDhCw1Du4LHCBaF17"
+    });
+
+    const create_payment_json = {
+      intent: "sale",
+      payer: {
+        payment_method: "paypal"
+      },
+      redirect_urls: {
+        return_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+      },
+      transactions: [
+        {
+          item_list: {
+            items: [
+              {
+                name: "Red sox hat",
+                sku: "001",
+                price: "1.00",
+                currency: "BRL",
+                quantity: 1
+              }
+            ]
+          },
+          amount: {
+            currency: "BRL",
+            total: "1.00"
+          },
+          description: "This is the payment description."
+        }
+      ]
+    };
+
+    paypal.payment.create(create_payment_json, function(error, payment) {
+      if (error) {
+        throw error;
+      } else {
+        for (let i = 0; i < payment.links.length; i++) {
+          if (payment.links[i].rel === "approval_url") {
+            // res.redirect(payment.links[i].href);
+          }
+        }
+        console.log(payment);
+      }
+    });
   },
+  async success(req, res) {
+    const { paymentId, token, PayerID } = req.query;
 
-  async all(req, res) {},
+    const execute_payment_json = {
+      payer_id: PayerID,
+      transactions: [
+        {
+          amount: {
+            currency: "BRL",
+            total: "1.00"
+          }
+        }
+      ]
+    };
 
-  async get(req, res) {}
+    paypal.payment.execute(paymentId, execute_payment_json, function(
+      error,
+      payment
+    ) {
+      if (error) {
+        console.log("error");
+      } else {
+        console.log("get payment");
+        console.log(JSON.stringify(payment));
+        return res.send(`${paymentId} e ${PayerID}`);
+      }
+    });
+  }
 };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const routes = new express.Router();
 
 const BuyersController = require("./app/controllers/BuyersController");
+const PageController = require("./app/controllers/PageController");
 
 routes.get("/buyers/get", BuyersController.getAll);
 routes.get("/buyers/get/:id", BuyersController.get);
@@ -10,5 +11,8 @@ routes.get("/buyers/update/:id", BuyersController.update);
 routes.get("/buyers/delete/:id", BuyersController.delete);
 
 routes.post("/new", BuyersController.store);
+
+routes.get("/pay", PageController.pay);
+routes.get("/success", PageController.success);
 
 module.exports = routes;
